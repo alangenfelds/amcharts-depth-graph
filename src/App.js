7ca65a3cbfc9ec84809fc1780e1805bff3b607ba
Langenfelds, Arturs <a.langenfelds@a1qa.com>
2020-04-01 18:23:06 +0300

    depth chart original

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import logo from './logo.svg';
 // import './App.css';
 import * as am4core from "@amcharts/amcharts4/core";
@@ -9,38 +9,127 @@ am4core.useTheme(am4themes_animated);
 
 class App extends Component {
   componentDidMount() {
+    // Create chart instance
     let chart = am4core.create("chartdiv", am4charts.XYChart);
 
-    chart.paddingRight = 40;
+    // Add data
+    chart.dataSource.url =
+      "https://poloniex.com/public?command=returnOrderBook&currencyPair=BTC_ETH&depth=50";
+    // chart.dataSource.reloadFrequency = 30000;
+    chart.dataSource.adapter.add("parsedData", function(data) {
+      // Function to process (sort and calculate cummulative volume)
+      function processData(list, type, desc) {
+        // Convert to data points
+        for (let i = 0; i < list.length; i++) {
+          list[i] = {
+            value: Number(list[i][0]),
+            volume: Number(list[i][1])
+          };
+        }
 
-    let data = [];
-    let visits = 10;
-    for (let i = 1; i < 366; i++) {
-      visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
-      data.push({ date: new Date(2018, 0, i), name: "name" + i, value: visits });
-    }
+        // Sort list just in case
+        list.sort(function(a, b) {
+          if (a.value > b.value) {
+            return 1;
+          } else if (a.value < b.value) {
+            return -1;
+          } else {
+            return 0;
+          }
+        });
 
-    chart.data = data;
+        // Calculate cummulative volume
+        if (desc) {
+          for (let i = list.length - 1; i >= 0; i--) {
+            if (i < list.length - 1) {
+              list[i].totalvolume = list[i + 1].totalvolume + list[i].volume;
+            } else {
+              list[i].totalvolume = list[i].volume;
+            }
 
-    let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
-    dateAxis.renderer.grid.template.location = 0;
+            const dp = {};
 
-    let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-    valueAxis.tooltip.disabled = true;
-    valueAxis.renderer.minWidth = 35;
+            dp["value"] = list[i].value;
+            dp[type + "volume"] = list[i].volume;
+            dp[type + "totalvolume"] = list[i].totalvolume;
+            res.unshift(dp);
+          }
+        } else {
+          for (let i = 0; i < list.length; i++) {
+            if (i > 0) {
+              list[i].totalvolume = list[i - 1].totalvolume + list[i].volume;
+            } else {
+              list[i].totalvolume = list[i].volume;
+            }
 
-    let series = chart.series.push(new am4charts.LineSeries());
-    series.dataFields.dateX = "date";
-    series.dataFields.valueY = "value";
+            const dp = {};
 
-    series.tooltipText = "{valueY.value}";
+            dp["value"] = list[i].value;
+            dp[type + "volume"] = list[i].volume;
+            dp[type + "totalvolume"] = list[i].totalvolume;
+            res.push(dp);
+          }
+        }
+      }
+
+      // Init
+      const res = [];
+      processData(data.bids, "bids", true);
+      processData(data.asks, "asks", false);
+      console.log('res', res)
+      return res;
+    });
+
+    // Set up precision for numbers
+    chart.numberFormatter.numberFormat = "#,###.####";
+
+    // Create axes
+    const xAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+    xAxis.dataFields.category = "value";
+    //xAxis.renderer.grid.template.location = 0;
+    xAxis.renderer.minGridDistance = 50;
+    // xAxis.title.text = "Price (BTC/ETH)";
+
+    const yAxis = chart.yAxes.push(new am4charts.ValueAxis());
+    yAxis.title.text = "Volume";
+
+    // Create series
+    let series = chart.series.push(new am4charts.StepLineSeries());
+    series.dataFields.categoryX = "value";
+    series.dataFields.valueY = "bidstotalvolume";
+    series.strokeWidth = 2;
+    series.stroke = am4core.color("#0f0");
+    series.fill = series.stroke;
+    series.fillOpacity = 0.2;
+    series.tooltipText =
+      "Ask: [bold]{categoryX}[/]\nTotal volume: [bold]{valueY}[/]\nVolume: [bold]{bidsvolume}[/]";
+
+    let series2 = chart.series.push(new am4charts.StepLineSeries());
+    series2.dataFields.categoryX = "value";
+    series2.dataFields.valueY = "askstotalvolume";
+    series2.strokeWidth = 2;
+    series2.stroke = am4core.color("#f00");
+    series2.fill = series2.stroke;
+    series2.fillOpacity = 0.1;
+    series2.tooltipText =
+      "Ask: [bold]{categoryX}[/]\nTotal volume: [bold]{valueY}[/]\nVolume: [bold]{asksvolume}[/]";
+
+    // let series3 = chart.series.push(new am4charts.ColumnSeries());
+    // series3.dataFields.categoryX = "value";
+    // series3.dataFields.valueY = "bidsvolume";
+    // series3.strokeWidth = 0;
+    // series3.fill = am4core.color("#000");
+    // series3.fillOpacity = 0.2;
+
+    // let series4 = chart.series.push(new am4charts.ColumnSeries());
+    // series4.dataFields.categoryX = "value";
+    // series4.dataFields.valueY = "asksvolume";
+    // series4.strokeWidth = 0;
+    // series4.fill = am4core.color("#000");
+    // series4.fillOpacity = 0.;
+
+    // Add cursor
     chart.cursor = new am4charts.XYCursor();
-
-    let scrollbarX = new am4charts.XYChartScrollbar();
-    scrollbarX.series.push(series);
-    chart.scrollbarX = scrollbarX;
-
-    this.chart = chart;
   }
 
   componentWillUnmount() {
@@ -50,9 +139,7 @@ class App extends Component {
   }
 
   render() {
-    return (
-      <div id="chartdiv" style={{ width: "100%", height: "500px" }}></div>
-    );
+    return <div id="chartdiv" style={{ width: "100%", height: "500px" }}></div>;
   }
 }
 
